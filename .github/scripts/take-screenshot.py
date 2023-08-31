9917095c165af62bc519aab3269696ede6148472
Once-a-deadcat <catgorogoro994@gmail.com>
2023-08-31 19:34:13 +0900

    [modify] layout

--- .github/scripts/take-screenshot.py
+++ .github/scripts/take-screenshot.py
@@ -4,8 +4,6 @@ import time
 from PIL import Image
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.keys import Keys
 
 def take_screenshot():
     # Chromeのオプション設定
@@ -22,21 +20,19 @@ def take_screenshot():
         # ウェブサイトにアクセス
         driver.get('https://honzaap.github.io/GithubCity/?name=Once-a-deadcat&year=2023')
 
-        driver.implicitly_wait(15)
+        # ウェブサイトがロードされるまで待つ
+        driver.implicitly_wait(10)
 
         # スクリーンショットを連続して撮影
         screenshots = []
-        action = ActionChains(driver)
-        for i in range(60):  # 6秒間、0.1秒ごとにスクリーンショットを撮影
-            # ズームアウト
-            action.send_keys(Keys.COMMAND, Keys.SUBTRACT).perform()  # これはMacの場合です。Windowsの場合は、Keys.CONTROLを使用してください。
+        for _ in range(100):  # 6秒間、0.05秒ごとにスクリーンショットを撮影
             screenshot = driver.get_screenshot_as_png()
             screenshots.append(screenshot)
             time.sleep(0.05)
 
         # 連続したスクリーンショットからGIFを作成
         images = [Image.open(io.BytesIO(screenshot)) for screenshot in screenshots]
-        images[0].save('screenshot.gif', save_all=True, append_images=images[1:], loop=0, duration=100)
+        images[0].save('screenshot.gif', save_all=True, append_images=images[1:], loop=0, duration=200)
 
     finally:
         driver.quit()
