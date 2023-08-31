be12e79332ad27bb88932a1b27fe6351f0bc80c6
Once-a-deadcat <catgorogoro994@gmail.com>
2023-08-31 17:30:47 +0900

    [modfiy] launch option

--- .github/scripts/take-screenshot.js
+++ .github/scripts/take-screenshot.js
@@ -1,9 +1,9 @@
 const puppeteer = require('puppeteer');
 
 async function run() {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox']});
   const page = await browser.newPage();
-  await page.goto('https://honzaap.github.io/GithubCity/?name=Once-a-deadcat&year=2023', {waitUntil: 'networkidle2', timeout: 60000});
+  await page.goto('https://honzaap.github.io/GithubCity/?name=Once-a-deadcat&year=2023', {waitUntil: 'networkidle2', timeout: 180000});
   await page.screenshot({path: 'screenshot.png'});
 
   await browser.close();
