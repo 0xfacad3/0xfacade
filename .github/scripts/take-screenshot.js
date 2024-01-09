4903b0effdee2628dde2235ccba1514524ad0ee7
ImCat <65708470+imcat0131@users.noreply.github.com>
2024-01-09 23:25:37 +0900

    Update take-screenshot.js

--- .github/scripts/take-screenshot.js
+++ .github/scripts/take-screenshot.js
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer');
 async function run() {
   const browser = await puppeteer.launch({headless: true, executablePath: '/usr/bin/chromium-browser',args: ['--no-sandbox', '--disable-setuid-sandbox']});
   const page = await browser.newPage();
-  await page.goto('https://honzaap.github.io/GithubCity/?name=Once-a-deadcat&year=2023', {waitUntil: 'networkidle2', timeout: 1800000});
+  await page.goto('https://honzaap.github.io/GithubCity/?name=Once-a-deadcat&year=2024', {waitUntil: 'networkidle2', timeout: 1800000});
   await page.screenshot({path: 'screenshot.png'});
 
   await browser.close();
